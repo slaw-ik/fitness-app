dd8e4c4aac053a4132a70ade1c321720f1e7a0ba
Myroslav Nastenko <nastenko@essential-data.sk>
2019-11-05 22:17:20 +0100

    initial commit

---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const Title: React.FunctionComponent = props => (
+  <Typography component="h2" variant="h6" color="primary" gutterBottom>
+    {props.children}
+  </Typography>
+);
+
+export default Title;

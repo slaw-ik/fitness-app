917709be25ff847e71558d97b1b0bf344ee45408
Myroslav Nastenko <nastenko@essential-data.sk>
2019-11-18 23:17:58 +0100

    Simple Firebase

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
@@ -21,6 +21,8 @@ import Orders from './Orders';
 import { useStyles } from './styles';
 import Button from '@material-ui/core/Button';
 import LoginDialog from './LoginDialog';
+import fire from '../config/fire';
+import firebase from 'firebase';
 
 const Copyright: React.FunctionComponent = () => (
   <Typography variant="body2" color="textSecondary" align="center">
@@ -35,8 +37,11 @@ const Copyright: React.FunctionComponent = () => (
 
 const App: React.FunctionComponent = () => {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const [loginOpen, setLoginOpen] = React.useState(false);
+  const [open, setOpen] = useState(true);
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [user, setUser] = useState<firebase.User | null>(null);
+
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -50,7 +55,60 @@ const App: React.FunctionComponent = () => {
   const handleLoginClose = () => {
     setLoginOpen(false);
   };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  const handleLogin = () => {
+    const email = 'foo@bar.baz';
+    const password = 'superpass';
+
+    // fire
+    //   .auth()
+    //   .signInWithEmailAndPassword(email, password)
+    //   .then(u => {
+    //     console.log(u);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    fire.auth().signInWithPopup(provider);
+
+  };
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  };
+
+  const renderLoginButtons = () => {
+    if (user) {
+      return (
+        <Button color="inherit" onClick={handleLogout}>
+          TEST Logout
+        </Button>
+      );
+    } else {
+      return (
+        <Button color="inherit" onClick={handleLogin}>
+          TEST Login
+        </Button>
+      );
+    }
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
 
   return (
     <div className={classes.root}>
@@ -72,6 +130,7 @@ const App: React.FunctionComponent = () => {
           <Button color="inherit" onClick={handleLoginOpen}>
             Login
           </Button>
+          {renderLoginButtons()}
           <LoginDialog open={loginOpen} handleClose={handleLoginClose} />
         </Toolbar>
       </AppBar>
